a575f672d84274143ced2147688ef4c10c1f17a8
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2022-03-08 09:21:27 -0700

    Create money.py

---
+++ money.py
@@ -0,0 +1,13 @@
+def MakeChange(a):
+    print("number of quarters:",int(a/25))
+    b=a%25
+    print("number of dimes:",int(b/10))
+    c=b%25
+    print("number of nickels:",int(c/5))
+    d=c%25
+    print("number of pennies:",int(d/1))
+    
+
+print("Give an amount of money")
+money=int(input())
+MakeChange(money)
